559366822336bb6a86e673bda38e9a4e89786824
umarofz <webnajottalim@gmail.com>
2022-07-10 15:22:02 +0500

    changes made

--- js/main.js
+++ js/main.js
@@ -2,34 +2,24 @@ var elForm = document.querySelector(".form")
 var elInput = document.querySelector(".form__input")
 var elSelect = document.querySelector(".form__select")
 var elDiv = document.querySelector(".div")
-var elRub = document.querySelector(".rub__option")
-var elEur = document.querySelector(".eur__option")
+var eur = 11138.18;
+var dollar = 10912.30;
+var rub = 172.46;
 
 elForm.addEventListener("submit", function (evt) {
-  evt.preventDefault()
+    evt.preventDefault()
 
-  var inputValue = elInput.value.trim()
-})
-
-elSelect.addEventListener("change", function () {
-  var selectValue = elSelect.value
-
-
-})
-
-elInput.addEventListener("input", function () {
-  var inputValue = elInput.value.trim()
-  var euro = 11138.18;
-  var dollar = 10912.30;
-  var result = (inputValue * dollar);
-
-  elDiv.textContent = result;
-
-  elRub.addEventListener("change", function () {
     var inputValue = elInput.value.trim()
-    var rub = 172.46;
-    var result = (inputValue * rub);
 
-    elDiv.textContent = result;
-  })
+    switch (elSelect.value) {
+        case "USD":
+        elDiv.value = (inputValue * dollar).toFixed(2);
+        break;
+        case "RUB":
+        elDiv.value = (inputValue * rub).toFixed(2);
+        break;
+        case "EUR":
+        elDiv.value = (inputValue * eur).toFixed(2);
+        break;
+    }
 })
